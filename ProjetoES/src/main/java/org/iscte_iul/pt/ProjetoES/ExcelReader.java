8ba61a02221023006ceeefc7ade70d2186b2e6c5
Pedro_Aguas <pmfva1@iscte-iul.pt>
2020-11-12 17:51:00 +0000

    Iterating over Rows and Columns using Iterator
    Exception in thread "AWT-EventQueue-0" java.lang.NullPointerException:
    Cannot load from object array because "this.dados" is null
    	at org.iscte_iul.pt.ProjetoES.ExcelReader.StartRead(ExcelReader.java:50)
    	at org.iscte_iul.pt.ProjetoES.ExcelReader.<init>(ExcelReader.java:23)
    	at org.iscte_iul.pt.ProjetoES.GUI$1.actionPerformed(GUI.java:52)

--- ProjetoES/src/main/java/org/iscte_iul/pt/ProjetoES/ExcelReader.java
+++ ProjetoES/src/main/java/org/iscte_iul/pt/ProjetoES/ExcelReader.java
@@ -1,122 +1,66 @@
 package org.iscte_iul.pt.ProjetoES;
+
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+
+import java.awt.BorderLayout;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class ExcelReader {
-    public static final String SAMPLE_XLSX_FILE_PATH = "C:\\Users\\mdmpe\\OneDrive\\Documentos\\Defeitos.xlsx";
+	// private static String SAMPLE_XLSX_FILE_PATH =
+	// "C:\\Users\\pedro\\OneDrive\\Desktop\\Faculdade\\3º Ano\\Engenharia de
+	// Software\\Defeitos.xlsx";
+	private String PATH;
+	private String dados[][];
 
-    private static String dados[][];
-  
-    
-   
-
-    public static void main(String[] args) throws IOException, InvalidFormatException {
-
-        // Creating a Workbook from an Excel file (.xls or .xlsx)
-        Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
-
-        // Retrieving the number of sheets in the Workbook
-        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
-
-        /*
-           =============================================================
-           Iterating over all the sheets in the workbook (Multiple ways)
-           =============================================================
-        */
-
-        // 1. You can obtain a sheetIterator and iterate over it
-        Iterator<Sheet> sheetIterator = workbook.sheetIterator();
-        System.out.println("Retrieving Sheets using Iterator");
-        while (sheetIterator.hasNext()) {
-            Sheet sheet = sheetIterator.next();
-            System.out.println("=> " + sheet.getSheetName());
-        }
-
-        // 2. Or you can use a for-each loop
-        System.out.println("Retrieving Sheets using for-each loop");
-        for(Sheet sheet: workbook) {
-            System.out.println("=> " + sheet.getSheetName());
-        }
-
-        // 3. Or you can use a Java 8 forEach with lambda
-        System.out.println("Retrieving Sheets using Java 8 forEach with lambda");
-        workbook.forEach(sheet -> {
-            System.out.println("=> " + sheet.getSheetName());
-        });
-
-        /*
-           ==================================================================
-           Iterating over all the rows and columns in a Sheet (Multiple ways)
-           ==================================================================
-        */
-
-        // Getting the Sheet at index zero
-        Sheet sheet = workbook.getSheetAt(0);
-
-        // Create a DataFormatter to format and get each cell's value as String
-        DataFormatter dataFormatter = new DataFormatter();
-
-        // 1. You can obtain a rowIterator and columnIterator and iterate over them
-        System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
-        Iterator<Row> rowIterator = sheet.rowIterator();
-        while (rowIterator.hasNext()) {
-            Row row = rowIterator.next();
-            
-            // Now let's iterate over the columns of the current row
-            Iterator<Cell> cellIterator = row.cellIterator();
-            
-            
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-                String cellValue = dataFormatter.formatCellValue(cell);
-                
-                dados[row.getRowNum()][cell.getColumnIndex()]=cellValue;
-            }
-            //System.out.println();
-          
-        }
-
-        // 2. Or you can use a for-each loop to iterate over the rows and columns
-     /*   System.out.println("\n\nIterating over Rows and Columns using for-each loop\n");
-        for (Row row: sheet) {
-            for(Cell cell: row) {
-                String cellValue = dataFormatter.formatCellValue(cell);
-                System.out.print(cellValue + "\t");
-            }
-            System.out.println();
-        }*/
-
-        // 3. Or you can use Java 8 forEach loop with lambda
-       /* System.out.println("\n\nIterating over Rows and Columns using Java 8 forEach with lambda\n");
-        sheet.forEach(row -> {
-            row.forEach(cell -> {
-                String cellValue = dataFormatter.formatCellValue(cell);
-                System.out.print(cellValue + "\t");
-            });
-            System.out.println();
-        });
-
-        // Closing the workbook
-         * 
-        workbook.close();*/
-       
-    }
-
-
-
-
-	public static String getSampleXlsxFilePath() {
-		return SAMPLE_XLSX_FILE_PATH;
+	public ExcelReader(String PATH) throws InvalidFormatException, IOException{
+		this.PATH = PATH;
+		StartRead();
 	}
 
+	public void StartRead() throws IOException, InvalidFormatException {
 
+		// Creating a Workbook from an Excel file (.xls or .xlsx)
+		Workbook workbook = WorkbookFactory.create(new File(PATH));
 
+		// Getting the Sheet at index zero
+		Sheet sheet = workbook.getSheetAt(0);
 
-	public static String[][] getDados() {
+		// Create a DataFormatter to format and get each cell's value as String
+		DataFormatter dataFormatter = new DataFormatter();
+
+		// 1. You can obtain a rowIterator and columnIterator and iterate over them
+		System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
+		Iterator<Row> rowIterator = sheet.rowIterator();
+		while (rowIterator.hasNext()) {
+			Row row = rowIterator.next();
+
+			// Now let's iterate over the columns of the current row
+			Iterator<Cell> cellIterator = row.cellIterator();
+
+			while (cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
+				String cellValue = dataFormatter.formatCellValue(cell);
+
+				dados[row.getRowNum()][cell.getColumnIndex()] = cellValue;
+			}
+			// System.out.println();
+
+		}
+
+	}
+
+	public String getSampleXlsxFilePath() {
+		return PATH;
+	}
+
+	public String[][] getDados() {
 		return dados;
 	}
-    
+
 }
--- ProjetoES/src/main/java/org/iscte_iul/pt/ProjetoES/GUI.java
+++ ProjetoES/src/main/java/org/iscte_iul/pt/ProjetoES/GUI.java
@@ -1,200 +1,91 @@
 package org.iscte_iul.pt.ProjetoES;
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.table.TableColumnModel;
-import javax.swing.table.TableModel;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 public class GUI {
 	private JFrame frame;
+	private String Path = "C:\\Users\\pedro\\OneDrive\\Desktop\\Faculdade\\3º Ano\\Engenharia de Software\\Defeitos.xlsx";
+	private ExcelReader ER;
 	private File selectedFile;
-	private File selectedImg;
-	private File[] ArrayInicial;
-	private ArrayList<BufferedImage> ArrayImg = new ArrayList<>();
 	private String path;
-	private ArrayList<String> ArrayStrings = new ArrayList<>(); 
-	private ArrayList<byte[]> ArrayByte = new ArrayList<byte[]>(); 
-	@SuppressWarnings("rawtypes")
-	private DefaultListModel model = new DefaultListModel<String>();
-	
-	
-	
 
-	public GUI(){
-		frame = new JFrame ("Find Images");
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); 
+	public GUI() {
+		frame = new JFrame("Read Excel");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
 		configurationFrame();
 		frame.setSize(700, 450);
-		   
-	}	
-
-	public void open(){
-		frame.setVisible(true);
 	}
 
-	
-	
-	private void configurationFrame(){
-
-		ArrayList<String> listaWorkers = new ArrayList<>();
-		listaWorkers.add("Procura normal");
-		//		listaWorkers.add("tarefa2");
-		//		listaWorkers.add("tarefa3");
-		//		listaWorkers.add("tarefa4");
-		//imagens.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-
-		JList<String> tarefas = new JList(listaWorkers.toArray());
-		frame.add (tarefas, BorderLayout.WEST); 
-
-		final JList<String> imagens = new JList<String>(model);
-		final JLabel labelImage = new JLabel();
-		imagens.addListSelectionListener(new ListSelectionListener() {
-			@Override
-			public void valueChanged(ListSelectionEvent e) {
-				if (e.getValueIsAdjusting()){
-					System.out.println(imagens.getSelectedIndex());
-					int i=imagens.getSelectedIndex();
-
-					System.out.println("Index no Array " + i);
-					BufferedImage b=ArrayImg.get(i);
-					System.out.println("BufferedImage " + b);
-
-					ImageIcon c = new ImageIcon(b);
-					labelImage.setIcon(c);
-				}
-			}
-		});
-
-
-
-		frame.add (imagens, BorderLayout.EAST); 
-
-	//Parte onde o excel vai ficar
-		
-		
-		
-		JTable jt=new JTable(ExcelReader.getDados(),new String[] {"1","1","1","1","1","1","1","1","1","10","11","12"}) ;    	  
-		JTextArea textArea = new JTextArea(20, 20);  
-		JScrollPane scrollableTextArea = new JScrollPane(jt);  
-
-		scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
-		scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);  
-
-		frame.getContentPane().add(scrollableTextArea);  
-
-		JPanel panel = new JPanel(new BorderLayout());
-
-		JPanel panelText = new JPanel();
-		panelText.setLayout(new GridLayout(2, 1));
-
-	//Fim do ScrollPane 
-		
-		
-		
-		JTextField textPasta = new JTextField();
-		JTextField textImage = new JTextField();
-
-	
-
-		
-		panelText.add (textPasta);
-		panelText.add (textImage);
-
-		panel.add (panelText, BorderLayout.CENTER);
-
+	private void configurationFrame() {
 
 		JPanel panelButton = new JPanel();
-		panelButton.setLayout(new GridLayout(2, 1));
-
-		JButton Pasta = new JButton ("Pasta");
-
-		Pasta.addActionListener(new ActionListener(){
+		panelButton.setLayout(new GridLayout(1, 2));
+		
+		JButton Excel = new JButton("Excel");
+		Excel.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent x) {
 				JFileChooser jfc = new JFileChooser(".");
-				jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
 				int returnValue = jfc.showOpenDialog(null);
 
 				if (returnValue == JFileChooser.APPROVE_OPTION) {
 					selectedFile = jfc.getSelectedFile();
-					textPasta.setText(selectedFile.getAbsolutePath());
 					path = selectedFile.getAbsolutePath();
-				}else{ 
-					if (returnValue == JFileChooser.CANCEL_OPTION){
+					try {
+						ER = new ExcelReader(path);
+					} catch (InvalidFormatException | IOException e) {
+						e.printStackTrace();
+					}
+					
+					
+					JTable jt = new JTable(ER.getDados(), 
+							new String[] { "1", "1", "1", "1", "1", "1", "1", "1", "1", "10", "11", "12" });
+
+					JScrollPane excel = new JScrollPane(jt);
+						
+					excel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+					excel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+					
+					
+					frame.add(excel, BorderLayout.CENTER);
+				} else {
+					if (returnValue == JFileChooser.CANCEL_OPTION) {
 						System.out.println("No File Selected");
 					}
 				}
 			}
 		});
-
-
-		JButton Imagem = new JButton ("Imagem");
-		Imagem.addActionListener(new ActionListener(){
-			public void actionPerformed (ActionEvent x){
-				JFileChooser jfc = new JFileChooser();
-				FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images", "jgp", "gif", "png");
-				jfc.addChoosableFileFilter(filter);
-				int returnValue = jfc.showOpenDialog(null);
-
-				if (returnValue == JFileChooser.APPROVE_OPTION){
-					selectedImg = jfc.getSelectedFile();
-					textImage.setText(selectedImg.getName());
-				}else if (returnValue == JFileChooser.CANCEL_OPTION){
-					System.out.println("No File Selected");
-				}
-			}
-		});
-
-		panelButton.add(Pasta);
-		panelButton.add(Imagem);
-
-		panel.add (panelButton, BorderLayout.EAST);
+		
+		
+		JButton Defeitos = new JButton("Defeitos");
+		panelButton.add(Defeitos);
+		panelButton.add(Excel);
+		frame.add(panelButton, BorderLayout.SOUTH);
+		
 	}
 
-/*
-		JButton Procurar = new JButton ("Procurar");
-		Procurar.addActionListener(new ActionListener(){
-			public void actionPerformed (ActionEvent x){
-				//try {
-					//ArrayImg();
-					//convertArrayImg();
-				//} catch (IOException e1) {
-					//e1.printStackTrace();
-				//}
-
-
-		//	}
-		//S});
-
-		panel.add(Procurar, BorderLayout.SOUTH);
-		frame.add (panel, BorderLayout.SOUTH);
+	public void open() {
+		frame.setVisible(true);
 	}
-		*/
-	
 
-
-
-	public static void main (String[] args) {
-			new GUI().open();
-
+	public static void main(String[] args) {
+		new GUI().open();
 	}
-	
 }
-
